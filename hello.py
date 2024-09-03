c9418cd0ba3f8e96bee50a305b9484961f756795
Bettayeb Riyad <riyad.bettayeb@polymtl.ca>
2024-09-03 17:39:30 -0400

    Réponse à la question 1 complétée

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+aire = math.pi * rayon ** 2
+print(f"l'aire du cercle avec un rayon de {rayon} est : {aire}")
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
